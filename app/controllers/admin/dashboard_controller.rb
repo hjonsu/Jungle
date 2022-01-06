c8c2fe377040a12d9b9a28ac6ae0f75c273dc933
Hang <jsu6898@gmail.com>
2022-01-05 23:54:24 -0800

    creates admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ require 'dotenv/load'
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["Admin_User"], :password => ENV["Admin_PW"]
   def show
+    @product_quantity = Product.count
+    @category_quantity = Category.count
   end
 end
