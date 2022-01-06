e59d2db3ca281cadc91a136c92f11c16f9efa554
Hang <jsu6898@gmail.com>
2022-01-05 23:37:02 -0800

    Admin Auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
+require 'dotenv/load'
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with :name => ENV["Admin_User"], :password => ENV["Admin_PW"]
   def show
   end
 end
